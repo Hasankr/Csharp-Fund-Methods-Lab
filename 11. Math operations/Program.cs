38966035ce832129a401b20e55edab3eb418491a
hasankr <hasankr@abv.bg>
2022-06-10 11:01:45 +0300

    added 11

---
+++ 11. Math operations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+            double result = Calculate(a, @operator, b);
+            Console.WriteLine(result);
+
+        }
+        static double Calculate(int a, string @operator, int b)
+        {
+            double result = 0;
+            if (@operator == "+")
+            {
+                result = a + b;
+            }
+            else if (@operator == "-")
+            {
+                result = a - b;
+            }
+            else if (@operator == "*")
+            {
+                result = a * b;
+            }
+            else if (@operator == "/")
+            {
+                result = a / b;
+            }
+            return result;
+        }
+    }
+}
