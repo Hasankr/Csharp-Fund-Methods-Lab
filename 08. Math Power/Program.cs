acf9312fd7b7df99b5164c65c2be5b2ff708bc97
hasankr <hasankr@abv.bg>
2022-06-10 08:32:36 +0300

    added 8.0

---
+++ 08. Math Power/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double @base = double.Parse(Console.ReadLine());
+            int pow = int.Parse(Console.ReadLine());
+            Console.WriteLine(MathPower(@base, pow));
+
+        }
+        static double MathPower(double @base, int pow)
+        {
+            double result = 1;
+            for (int i = 0; i < pow; i++)
+            {
+                 result *= @base;
+            }
+            return result;
+        }
+    }
+}
